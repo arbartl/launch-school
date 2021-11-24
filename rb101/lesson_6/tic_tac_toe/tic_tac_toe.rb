9645b11431811d990e8e6b33577982fa29411ade
Anthony <knoxbartl@gmail.com>
2021-11-24 12:18:37 -0600

    Added score tracking & computer AI

--- rb101/lesson_6/tic_tac_toe/tic_tac_toe.rb
+++ rb101/lesson_6/tic_tac_toe/tic_tac_toe.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
@@ -17,6 +19,22 @@ end
 
 def clear_screen
   system('clear')
+end
+
+def joinor(arr, separator=', ', last_separator='or')
+  last_element = arr.pop
+  arr.join(separator) + " #{last_separator} " + last_element.to_s
+end
+
+def get_points_to_win
+  points_to_win = nil
+  loop do
+    prompt("How many points do you want to play to? (1 - 10)")
+    points_to_win = gets.chomp.strip.to_i
+    break if (1..10).include?(points_to_win)
+    prompt("That's not a valid point goal...")
+  end
+  points_to_win
 end
 
 # rubocop:disable Metrics/AbcSize
@@ -55,7 +73,7 @@ end
 def player_places_piece!(board)
   player_choice = nil
   loop do
-    prompt("Choose an empty square (#{empty_squares(board).join(', ')})")
+    prompt("Choose an empty square (#{joinor(empty_squares(board))})")
     player_choice = gets.chomp.strip.to_i
     break if valid_choice?(player_choice, board)
     prompt("That's not a valid choice...")
@@ -64,6 +82,23 @@ def player_places_piece!(board)
 end
 
 def computer_places_piece!(board)
+  WINNING_LINES.each do |line|
+    if board.values_at(*line).count(PLAYER_MARKER) == 2
+      line.each do |index|
+        if board[index] == " "
+          computer_choice = index
+          return board[computer_choice] = COMPUTER_MARKER
+        end
+      end
+    elsif board.values_at(*line).count(COMPUTER_MARKER) == 2
+      line.each do |index|
+        if board[index] == " "
+          computer_choice = index
+          return board[computer_choice] = COMPUTER_MARKER
+        end
+      end      
+    end
+  end
   computer_choice = empty_squares(board).sample
   board[computer_choice] = COMPUTER_MARKER
 end
@@ -87,13 +122,26 @@ def detect_winner(board)
   nil
 end
 
-def display_winner(board)
+def display_round_winner(board)
   display_board(board)
   if someone_won?(board)
-    prompt("#{detect_winner(board)} won!")
+    prompt("#{detect_winner(board)} won a point!")
   else
     prompt("It's a tie!")
   end
+end
+
+def update_scores(winner, scores)
+  scores[winner.to_sym] += 1
+end
+
+def display_score(scores, points_to_win, board)
+  prompt("Player: #{scores[:Player]} -- Computer: #{scores[:Computer]}")
+  prompt("First to #{points_to_win} wins!") if !detect_winner(board)
+end
+
+def display_game_winner(scores, points_to_win)
+  prompt("#{scores.key(points_to_win).to_s} wins the game!")
 end
 
 def play_again
@@ -109,20 +157,36 @@ def play_again
 end
 
 loop do # main loop
-  board = initialize_board
+  scores = {
+    Player: 0,
+    Computer: 0
+  }
+  points_to_win = get_points_to_win
+  board = nil
 
   loop do
-    display_board(board)
+    board = initialize_board
 
-    player_places_piece!(board)
-    break if someone_won?(board)
+    loop do
+      display_board(board)
+      display_score(scores, points_to_win, board)
 
-    computer_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
+      player_places_piece!(board)
+      break if someone_won?(board)
+
+      computer_places_piece!(board)
+      break if someone_won?(board) || board_full?(board)
+    end
+
+    winner = detect_winner(board)
+    display_round_winner(board)
+    sleep(1.25)
+    update_scores(winner, scores) if winner
+    break if scores.values.any?(points_to_win)
   end
 
-  display_winner(board)
-
+  display_score(scores, points_to_win, board)
+  display_game_winner(scores, points_to_win)
   answer = play_again
   break unless VALID_EXIT_ANSWER[:positive].include?(answer)
 end
