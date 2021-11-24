e5c0227f51daae237ea38989eccd22225c632c7c
Anthony <knoxbartl@gmail.com>
2021-11-24 10:36:05 -0600

    Added base Tic Tac Toe functionality

--- rb101/lesson_6/tic_tac_toe/tic_tac_toe.rb
+++ rb101/lesson_6/tic_tac_toe/tic_tac_toe.rb
@@ -1,8 +1,27 @@
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
+
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
+                [[1, 5, 9], [3, 5, 7]]              # diagonals
+
+VALID_EXIT_ANSWER = {
+  positive: ['y', 'yes'],
+  negative: ['n', 'no']
+}
+
 def prompt(message)
   puts "=> #{message}"
 end
 
+def clear_screen
+  system('clear')
+end
+
+# rubocop:disable Metrics/AbcSize
 def display_board(board)
+  clear_screen
   puts ""
   puts "     |     |"
   puts "  #{board[1]}  |  #{board[2]}  |  #{board[3]}  "
@@ -17,15 +36,16 @@ def display_board(board)
   puts "     |     |"
   puts ""
 end
+# rubocop:enable Metrics/AbcSize
 
 def initialize_board
   board = {}
-  (1..9).each { |num| board[num] = ' ' }
+  (1..9).each { |num| board[num] = INITIAL_MARKER }
   board
 end
 
 def empty_squares(board)
-  board.keys.select { |num| board[num] }
+  board.keys.select { |num| board[num] == INITIAL_MARKER }
 end
 
 def valid_choice?(square, board)
@@ -36,28 +56,75 @@ def player_places_piece!(board)
   player_choice = nil
   loop do
     prompt("Choose an empty square (#{empty_squares(board).join(', ')})")
-    player_choice = gets.chomp.to_i
+    player_choice = gets.chomp.strip.to_i
     break if valid_choice?(player_choice, board)
     prompt("That's not a valid choice...")
   end
-  board[player_choice] = 'X'
+  board[player_choice] = PLAYER_MARKER
 end
 
 def computer_places_piece!(board)
   computer_choice = empty_squares(board).sample
-  board[computer_choice] = 'O'
+  board[computer_choice] = COMPUTER_MARKER
 end
 
+def someone_won?(board)
+  !!detect_winner(board)
+end
 
-board = initialize_board
+def board_full?(board)
+  empty_squares(board).empty?
+end
 
-display_board(board)
+def detect_winner(board)
+  WINNING_LINES.each do |line|
+    if board.values_at(*line).count(PLAYER_MARKER) == 3
+      return 'Player'
+    elsif board.values_at(*line).count(COMPUTER_MARKER) == 3
+      return 'Computer'
+    end
+  end
+  nil
+end
 
+def display_winner(board)
+  display_board(board)
+  if someone_won?(board)
+    prompt("#{detect_winner(board)} won!")
+  else
+    prompt("It's a tie!")
+  end
+end
 
-player_places_piece!(board)
+def play_again
+  answer = nil
+  loop do
+    prompt("Play again? (Y / N)")
+    answer = gets.chomp.downcase
+    break if VALID_EXIT_ANSWER[:positive].include?(answer) ||
+             VALID_EXIT_ANSWER[:negative].include?(answer)
+    prompt("Sorry, didn't catch that...")
+  end
+  answer
+end
 
-display_board(board)
+loop do # main loop
+  board = initialize_board
 
-computer_places_piece!(board)
+  loop do
+    display_board(board)
 
-display_board(board)
+    player_places_piece!(board)
+    break if someone_won?(board)
+
+    computer_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+  end
+
+  display_winner(board)
+
+  answer = play_again
+  break unless VALID_EXIT_ANSWER[:positive].include?(answer)
+end
+
+prompt("Thanks for playing!")
