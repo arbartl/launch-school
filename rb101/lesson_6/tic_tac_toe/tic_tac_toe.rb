b7a267ed70af6b0bf897457389be1cbe2e91f1e0
arbartl <knoxbartl@gmail.com>
2021-11-28 14:09:18 -0600

    Added difficulty selection with improved AI based on difficulty

--- rb101/lesson_6/tic_tac_toe/tic_tac_toe.rb
+++ rb101/lesson_6/tic_tac_toe/tic_tac_toe.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
@@ -22,6 +20,7 @@ def clear_screen
 end
 
 def joinor(arr, separator=', ', last_separator='or')
+  return arr[0].to_s if !arr[1]
   last_element = arr.pop
   arr.join(separator) + " #{last_separator} " + last_element.to_s
 end
@@ -35,6 +34,20 @@ def get_points_to_win
     prompt("That's not a valid point goal...")
   end
   points_to_win
+end
+
+def get_computer_difficulty
+  difficulty = nil
+  loop do
+    prompt("What difficulty do you want to face?")
+    prompt("1. Easy")
+    prompt("2. Medium")
+    prompt("3. Hard")
+    difficulty = gets.chomp.strip.to_i
+    break if (1..3).include?(difficulty)
+    prompt("That's not a valid difficulty selection...")
+  end
+  difficulty
 end
 
 # rubocop:disable Metrics/AbcSize
@@ -81,26 +94,42 @@ def player_places_piece!(board)
   board[player_choice] = PLAYER_MARKER
 end
 
-def computer_places_piece!(board)
+def find_at_risk_square(marker, board)
   WINNING_LINES.each do |line|
-    if board.values_at(*line).count(PLAYER_MARKER) == 2
+    if board.values_at(*line).count(marker) == 2
       line.each do |index|
         if board[index] == " "
-          computer_choice = index
-          return board[computer_choice] = COMPUTER_MARKER
+          return index
         end
       end
-    elsif board.values_at(*line).count(COMPUTER_MARKER) == 2
-      line.each do |index|
-        if board[index] == " "
-          computer_choice = index
-          return board[computer_choice] = COMPUTER_MARKER
-        end
-      end      
     end
   end
-  computer_choice = empty_squares(board).sample
-  board[computer_choice] = COMPUTER_MARKER
+  return nil
+end
+
+def computer_places_piece!(board, difficulty)
+  case difficulty
+  when 1
+    board[empty_squares(board).sample] = COMPUTER_MARKER
+  when 2
+    if find_at_risk_square(PLAYER_MARKER, board)
+      board[find_at_risk_square(PLAYER_MARKER, board)] = COMPUTER_MARKER
+    elsif find_at_risk_square(COMPUTER_MARKER, board)
+      board[find_at_risk_square(COMPUTER_MARKER, board)] = COMPUTER_MARKER
+    else
+      board[empty_squares(board).sample] = COMPUTER_MARKER
+    end
+  when 3
+    if find_at_risk_square(COMPUTER_MARKER, board)
+      board[find_at_risk_square(COMPUTER_MARKER, board)] = COMPUTER_MARKER
+    elsif find_at_risk_square(PLAYER_MARKER, board)
+      board[find_at_risk_square(PLAYER_MARKER, board)] = COMPUTER_MARKER
+    elsif board[5] == " "
+      board[5] = COMPUTER_MARKER
+    else
+      board[empty_squares(board).sample] = COMPUTER_MARKER
+    end
+  end
 end
 
 def someone_won?(board)
@@ -141,7 +170,7 @@ def display_score(scores, points_to_win, board)
 end
 
 def display_game_winner(scores, points_to_win)
-  prompt("#{scores.key(points_to_win).to_s} wins the game!")
+  prompt("#{scores.key(points_to_win)} wins the game!")
 end
 
 def play_again
@@ -157,11 +186,13 @@ def play_again
 end
 
 loop do # main loop
+  clear_screen
   scores = {
     Player: 0,
     Computer: 0
   }
   points_to_win = get_points_to_win
+  difficulty = get_computer_difficulty
   board = nil
 
   loop do
@@ -174,7 +205,7 @@ loop do # main loop
       player_places_piece!(board)
       break if someone_won?(board)
 
-      computer_places_piece!(board)
+      computer_places_piece!(board, difficulty)
       break if someone_won?(board) || board_full?(board)
     end
 
